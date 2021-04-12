756f4fc85ecc07d61711a1e9ae4f02f75455739c
Andres Benitez <abolnaits@hotmail.com>
2021-04-12 11:55:02 -0500

    App
    Get List by Ajax

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useEffect,useState} from 'react';
+import Recipe from './Recipe';
 
 function App(){
 
@@ -8,16 +9,36 @@ function App(){
     
     const [counter,setCounter] = useState(0);
 
+    const [recipes,setRecipes] = useState([]);
     //Funcion que se ejecuta cuando el estado de la app cambia
     useEffect(()=>{
         console.log('Effect has run');
         get_recipes();
     },[])
 
-    const get_recipes = async() =>{
+    
+    // Llamo a una funcion para obtener los datos via Ajax
+    const get_recipes_old = async() =>{
         const response = await fetch(url);
         const data = await response.json();
         console.log('Data ==>',data);
+         //Update state
+         setRecipes(data.hints);
+    }
+
+    const get_recipes = () =>{
+        console.log('Getting data');
+        fetch(url).then((response) => {
+            console.log('Get response');
+            return response.json();
+        }).then((data)=>{
+            console.log('Get data');
+            //Update state
+            setRecipes(data.hits);
+            console.log('Recipies => ',recipes);
+        }).catch(function(error){
+            console.log(error);
+        });
     }
     return(
         <div className="app">
@@ -27,6 +48,16 @@ function App(){
                 <input type="text" className="search_bar"/>
                 <button type="submit" onClick={()=>setCounter(counter+1)} className="search_btn">{counter}</button>
             </form>
+            <div>
+                {
+                    recipes.map((recipe)=>(
+                        <Recipe 
+                        title={recipe.recipe.label} 
+                        calories={recipe.recipe.calories} 
+                        image={recipe.recipe.image}></Recipe>
+                    ))
+                }
+            </div>
         </div>
     )
 }
