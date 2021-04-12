b4a07e83fc15ed4d8da18520d2c74579c482c094
Andres Benitez <abolnaits@hotmail.com>
2021-04-12 13:07:34 -0500

    App
    Comments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, {useEffect,useState} from 'react';
 import Recipe from './Recipe';
 
+//Main function
 function App(){
-
+    //Api credentials 
     const app_id = 'fbf69388';
     const app_key = '16c5027b9f08287d4f2b229a2c5c0f80';
     const url = `https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}`;
-    
+    //Set state helpers
+    //Pass the initial value into de useState function
     const [counter,setCounter] = useState(0);
-
     const [recipes,setRecipes] = useState([]);
     //Funcion que se ejecuta cuando el estado de la app cambia
     useEffect(()=>{
         console.log('Effect has run');
+        //Get the list of recipies , Ajax request
         get_recipes();
     },[])
 
@@ -40,13 +42,19 @@ function App(){
             console.log(error);
         });
     }
+    //Return the main App container
     return(
+        
         <div className="app">
             <h1>App Recetas</h1>
             <form className="search_form">
                 <label>Buscar:</label>
                 <input type="text" className="search_bar"/>
-                <button type="submit" onClick={()=>setCounter(counter+1)} className="search_btn">{counter}</button>
+                <button 
+                    type="submit" 
+                    onClick={()=>setCounter(counter+1)} 
+                    className="search_btn">{counter}
+                </button>
             </form>
             <div>
                 {
