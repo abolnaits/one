363f7c5277712db26acac6a51fbd17f396541c9a
Andres Benitez <abolnaits@hotmail.com>
2021-04-23 13:08:02 -0500

    App
    Search

--- src/App.js
+++ src/App.js
@@ -1,73 +1,84 @@
 import React, {useEffect,useState} from 'react';
 import Recipe from './Recipe';
 
-//Main function
-function App(){
-    //Api credentials 
+const App = () => {
+
+    //Constantes
     const app_id = 'fbf69388';
     const app_key = '16c5027b9f08287d4f2b229a2c5c0f80';
     const url = `https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}`;
-    //Set state helpers
-    //Pass the initial value into de useState function
+
     const [counter,setCounter] = useState(0);
     const [recipes,setRecipes] = useState([]);
-    //Funcion que se ejecuta cuando el estado de la app cambia
+    const [search,setSearch] = useState('');
+    //Query de busqueda por defecto
+    const [query,setQuery] = useState('chicken')
+
+    //Uso de useEffect
+
     useEffect(()=>{
-        console.log('Effect has run');
-        //Get the list of recipies , Ajax request
-        get_recipes();
-    },[])
-
-    
-    // Llamo a una funcion para obtener los datos via Ajax
-    const get_recipes_old = async() =>{
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log('Data ==>',data);
-         //Update state
-         setRecipes(data.hints);
-    }
-
-    const get_recipes = () =>{
-        console.log('Getting data');
-        fetch(url).then((response) => {
-            console.log('Get response');
+        console.log('useEffect run');
+        fetch(`https://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}`).then((response)=>{
+            console.log('Response =>',response);
+            //Devuelvo la repsuesta convertida 
             return response.json();
         }).then((data)=>{
-            console.log('Get data');
-            //Update state
+            //Manejo los datos
+            console.log('Data =>',data);
+            
+            
             setRecipes(data.hits);
-            console.log('Recipies => ',recipes);
-        }).catch(function(error){
-            console.log(error);
+            console.log('Recipes =>',recipes);
+        }).catch((error)=>{
+            console.error('Error =>',error);
         });
+    },[query]);
+
+    const getSearch = (e)=>{
+        console.log('Search =>',e);
+        e.preventDefault();
+        setQuery(search);
+        setSearch('');
     }
-    //Return the main App container
+
     return(
-        
-        <div className="app">
-            <h1>App Recetas</h1>
-            <form className="search_form">
-                <label>Buscar:</label>
-                <input type="text" className="search_bar"/>
-                <button 
-                    type="submit" 
-                    onClick={()=>setCounter(counter+1)} 
-                    className="search_btn">{counter}
-                </button>
+        <div className="App">
+            <h1>Search for food:</h1>
+            <form className="search-form">
+            <input 
+            className="search-bar"
+            value={search} 
+            onChange={(e)=>{
+                console.log('Event =>',e);
+                console.log('Value =>',e.target.value);
+                setSearch(e.target.value)
+            }}
+            />
+            <button 
+                className="search-button" 
+                onClick={getSearch}
+                type="submit">
+                Search
+            </button>
+
+            <button className="add-button" type="button" onClick={()=>{
+                //setCounter(counter + 1)
+            }}>Add{counter}</button>
             </form>
             <div>
                 {
-                    recipes.map((recipe)=>(
-                        <Recipe 
-                        title={recipe.recipe.label} 
-                        calories={recipe.recipe.calories} 
-                        image={recipe.recipe.image}></Recipe>
+                    recipes.map((recipe,i) =>(
+                        <Recipe key={i} i_k={i}
+                        title={recipe.recipe.label}
+                        calories={recipe.recipe.calories}
+                        image={recipe.recipe.image}
+                        ingredients={recipe.recipe.ingredients}
+                        ></Recipe>
                     ))
                 }
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default App;
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const Recipe = (props) => {
+const Recipe = (props) =>{
     return(
-        <div>
-            <h1>{props.title}</h1>
+        <div id={props.i_k}>
+            <h2>Title: { props.title }</h2>
             <p>Calories: {props.calories}</p>
-            <img src={props.image} />
+            <img src={props.image} alt=""/>
+            <h3>Ingredients: </h3>
+            {
+                props.ingredients.map((item)=>(
+                    1
+                ))
+            }
         </div>
-    )
+    );
 }
 
 export default Recipe;
